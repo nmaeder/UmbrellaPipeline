4d4739efbc864ec1bbc6960227e614a407ce95d8
wiederm <marcus.wieder@univie.ac.at>
2021-11-11 12:43:39 +0100

    only formatter changes

--- UmbrellaPipeline/pathGeneration/node.py
+++ UmbrellaPipeline/pathGeneration/node.py
@@ -1,18 +1,26 @@
 from typing import List
 
-class Node:
 
-    def __init__(self, x: int = -1, y: int = -1, z: int = -1, f: float = float('inf'), g: float = float('inf'), h: float = float('inf')):
+class Node:
+    def __init__(
+        self,
+        x: int = -1,
+        y: int = -1,
+        z: int = -1,
+        f: float = float("inf"),
+        g: float = float("inf"),
+        h: float = float("inf"),
+    ):
         self.x = x
         self.y = y
         self.z = z
         self.f = f
-        self.g = g 
+        self.g = g
         self.h = h
-        self.parent:Node = None 
+        self.parent: Node = None
 
     @classmethod
-    def fromCoords(cls, coords:List[int]):
+    def fromCoords(cls, coords: List[int]):
         """
         constructor creates node form list of node coordinates
 
@@ -21,15 +29,15 @@ class Node:
         """
 
         x, y, z = coords[0], coords[1], coords[2]
-        return cls(x,y,z)        
+        return cls(x, y, z)
 
     def __str__(self) -> str:
-        return f'[{self.x},{self.y},{self.z}]'
+        return f"[{self.x},{self.y},{self.z}]"
 
     def __repr__(self) -> str:
-        return f'[{self.x},{self.y},{self.z}]'
+        return f"[{self.x},{self.y},{self.z}]"
 
-    def __eq__(self, o:object) -> bool:
+    def __eq__(self, o: object) -> bool:
         return self.getCoordinates() == o.getCoordinates()
 
     def getCoordinates(self) -> List[int]:
