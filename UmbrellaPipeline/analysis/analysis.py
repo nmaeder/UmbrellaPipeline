eccbf52767fe874cfaea8a983c1c814bb4f389f9
Niels Maeder <maedern@student.ethz.ch>
2021-12-09 14:50:41 +0100

    a

--- UmbrellaPipeline/analysis/analysis.py
+++ UmbrellaPipeline/analysis/analysis.py
@@ -9,7 +9,9 @@ import numpy as np
 import openmm.unit as unit
 import mdtraj
 import torch
-from FastMBAR import FastMBAR, fastmbar
+from FastMBAR import FastMBAR
+import matplotlib.pyplot as plt
+
 
 class PMFCalculator:
     def __init__(
@@ -19,6 +21,7 @@ class PMFCalculator:
         number_of_bins: int,
         path: List[unit.Quantity],
         trajectory_directory: str,
+        path_interval: unit.Quantity,
     ) -> None:
         self.simulation_properties = simulation_properties
         self.system_info = simulation_system
@@ -29,18 +32,23 @@ class PMFCalculator:
             shape=(
                 len(self.path),
                 self.simulation_properties.number_of_rounds,
-                ),
+            ),
             dtype=unit.Quantity,
         )
         self.center_pmf: List[float]
         self.pmf: List[float]
         self.distance: List[unit.Quantity] = []
+        self.path_interval = path_interval
 
     def parse_trajectories(self):
         for i in range(len(self.path)):
             trajectory = mdtraj.load_dcd(f"{self.trajectory_directory}/traj_{i}_.dcd")
             for frame in range(self.simulation_properties.number_of_rounds):
-                self.coordinates[i][frame] = get_centroid_coordinates(trajectory[frame], self.system_info.psf_object.atom_list, self.system_info.ligand_indices)
+                self.coordinates[i][frame] = get_centroid_coordinates(
+                    trajectory[frame],
+                    self.system_info.psf_object.atom_list,
+                    self.system_info.ligand_indices,
+                )
                 self.distance.append(
                     math.sqrt(
                         (self.coordinates[i][frame].x - self.path[i].x) ** 2
@@ -50,18 +58,58 @@ class PMFCalculator:
                 )
 
     def calculate_pmf(self):
-        A = np.zeros((len(self.path), len(self.path) * self.simulation_properties.number_of_rounds))
-        kbT = (unit.BOLTZMANN_CONSTANT_kB * self.simulation_properties.temperature * unit.AVOGADRO_CONSTANT_NA).value_in_unit(unit.kilocalorie_per_mole)
-        num_conf=[]
+        A = np.zeros(
+            (
+                len(self.path),
+                len(self.path) * self.simulation_properties.number_of_rounds,
+            )
+        )
+        kbT = (
+            unit.BOLTZMANN_CONSTANT_kB
+            * self.simulation_properties.temperature
+            * unit.AVOGADRO_CONSTANT_NA
+        ).value_in_unit(unit.kilocalorie_per_mole)
+        num_conf = []
         for window in range(len(self.path)):
             dx = abs(self.coordinates[0:-1][0] - self.path[window].x)
             dy = abs(self.coordinates[0:-1][1] - self.path[window].y)
             dz = abs(self.coordinates[0:-1][2] - self.path[window].z)
-            A[window, :] = 0.5 * self.simulation_properties.force_constant*(dx**2 + dy**2 + dz**2)
+            A[window, :] = (
+                0.5
+                * self.simulation_properties.force_constant
+                * (dx ** 2 + dy ** 2 + dz ** 2)
+            )
             num_conf.append(self.simulation_properties.number_of_rounds)
         num_conf = np.array(num_conf).astype(np.float64)
-        wham = FastMBAR(energy=A, num_conf=num_conf, cuda=torch.cuda.is_available(), verbose=True)
-        
+        wham = FastMBAR(
+            energy=A, num_conf=num_conf, cuda=torch.cuda.is_available(), verbose=True
+        )
+        width = self.path_interval
+        center_pmf = np.linspace(
+            0, len(self.path) * width, self.number_of_bins, endpoint=False
+        )
+        B = np.zeros(
+            (
+                self.number_of_bins,
+                len(self.path) * self.simulation_properties.number_of_rounds,
+            )
+        )
+        for i in range(self.number_of_bins):
+            center = center_pmf[i]
+            center_low = center - 0.5 * width
+            center_high = center + 0.5 * width
+            indicator = (self.distance > center_low) & (self.distance <= center_high)
+            B[i, ~indicator] = np.inf
+        self.pmf, _ = wham.calculate_free_energies_of_perturbed_states(B)
 
     def create_pdf(self, filename: str):
-        pass
+        fig = plt.figure(0)
+        fig.clf()
+        plt.plot(self.center_pmf, self.pmf, "-o")
+        plt.xlim(
+            0,
+            len(self.path * self.path_interval.value_in_unit(self.path_interval.unit)),
+        )
+        plt.xlabel("Ligand distance from binding pocked [nm]")
+        plt.ylabel("Relative free energy [kcal per mole]")
+        plt.savefig(filename)
