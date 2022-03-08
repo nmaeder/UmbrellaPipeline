f4436a8d1eb8c1e581c4b1083965f81f3fc668a1
nmaeder <maedern@student.ethz.ch>
2022-03-08 22:16:04 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath(".."))
-import UmbrellaPipeline
+sys.path.insert(0, os.path.abspath("../UmbrellaPipeline/"))
+
 
 
 # -- Project information -----------------------------------------------------
